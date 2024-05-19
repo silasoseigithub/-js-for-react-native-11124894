1e1603c803bee61f0b7e5a9e4f0bba77ce987f54
silasoseigithub <sosei125@st.ug.edu.gh>
2024-05-19 01:31:03 -0700

    Update userInfo.js

--- userInfo.js
+++ userInfo.js
@@ -1,13 +1,28 @@
-function createUserProfiles(names, modifiedNames) {
+const {processArray, formatArrayStrings} =require('./arrayManipulation.js');
+
+function createUserProfiles(names) {
+  const processedNumbers = processArray(names);
+  const formatArrayStrings = formatArrayStrings(names, processedNumbers);
+
+  console.log(processedNumbers);
+  console.log(modifiedStrings);
+
     let id = 1;
     return names.map((name, index) => ({
       originalName: name,
       modifiedName: modifiedNames[index],
       id:id++,}));
   }
- const names=["Alice", "Bob","Silas"];
- const userprofiles=createUserProfiles(names,formattedStrings);
+ const names = ["Osei", "Peter","Silas"];
+const userprofiles = createUserProfiles(names);
  console.log(userprofiles);
+
+ 
+
+  
+ 
+
+ 
 
 
 // Expected output:
